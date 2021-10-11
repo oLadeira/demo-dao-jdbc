fb9f41a7d149e404e46717ca3fffb486ae10249f
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-10-11 10:20:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,20 @@
 package application;
 
+import java.util.Date;
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        
+        Department d1 = new Department(12, "Marketing");
+        
+        Seller s1 = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00);
+        
+        System.out.println(d1);
+        System.out.println();
+        System.out.println(s1);
         
     }
     
