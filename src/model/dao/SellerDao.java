6ef4b4088974ce6399264cae960f3a3b39a3cfab
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-10-11 10:28:18 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+    
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id); 
+    List<Seller> findAll();
+    
+}
